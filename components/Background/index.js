7180f65d2af863da7a5740e145cdbe177a017feb
Hope Kumordzie <officialhopek42@gmail.com>
2021-07-08 03:11:01 +0800

    fouc fix

--- components/Background/index.js
+++ components/Background/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { ImageContainer, Bg } from './backgroundStyles';
 
-const Background = ({loaded}) => {
+const Background = () => {
 
     return (
         <>
-            {
-                loaded &&
-                <>
-                    <Bg/>
-                    <ImageContainer loop='loop' autoPlay='autoPlay' muted='muted' src='/header.mp4' type='video/mp4' />
-                </>
-            
-            }
+
+
+
+            <Bg />
+            <ImageContainer loop='loop' autoPlay='autoPlay' muted='muted' src='/header.mp4' type='video/mp4' />
+
+
+
         </>
 
     )
